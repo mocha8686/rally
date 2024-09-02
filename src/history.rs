34e0afbbccba0abf0d6453f471aa771613836709
mocha8686 <june.mocha8686@gmail.com>
2024-09-01 19:24:16 -0700

    use cargo pkg name

--- src/history.rs
+++ src/history.rs
@@ -3,7 +3,7 @@ use std::{fs::create_dir_all, path::PathBuf};
 use directories::ProjectDirs;
 
 pub fn get_history_path(name: &str) -> Option<PathBuf> {
-    let dirs = ProjectDirs::from("", "", "rally")?;
+    let dirs = ProjectDirs::from("", "", env!("CARGO_PKG_NAME"))?;
     let dir = dirs.data_dir();
     create_dir_all(dir).ok();
     let dir = dir.join(format!("{name}_history"));
